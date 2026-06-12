604d2ef8286679ff3bda412d8fc5227f180b4908
tirosko <tibor.rosko@hotmail.com>
2026-06-12 14:30:00 +0200

    AppStart

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
-todos = []  # jednoduchý zoznam úloh v pamäti
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
-@app.route("/")
-def index():
-    return render_template("index.html", todos=todos)
-
-@app.route("/add", methods=["POST"])
-def add():
-    text = request.form.get("todo")
-    if text:
-        todos.append({"text": text, "done": False})
-    return redirect(url_for("index"))
-
-@app.route("/toggle/<int:idx>")
-def toggle(idx):
-    if 0 <= idx < len(todos):
-        todos[idx]["done"] = not todos[idx]["done"]
-    return redirect(url_for("index"))
-
-@app.route("/delete/<int:idx>")
-def delete(idx):
-    if 0 <= idx < len(todos):
-        todos.pop(idx)
-    return redirect(url_for("index"))
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
